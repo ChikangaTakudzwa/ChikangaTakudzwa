2ca7d8a0e9a756080669d44fa9b2537757bb33fb
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-03-03 23:15:43 +0000

    Sidebar working fine, left with adding annimation on hamburger

---
+++ src/assets/js/sidebar.js
@@ -0,0 +1,77 @@
+const navigation = `
+<section class="">
+  <div class="flex flex-col items-center mt-6 -mx-2 mb-3">
+      <img class="object-cover w-24 h-24 mx-2 rounded-full" src="https://ivesexid.sirv.com/Portfolio/avi.png" alt="avatar">
+      <h4 class="mx-2 mt-2 font-medium dark:text-gray-200">Chikanga Takudzwa</h4>
+      <p class="mx-2 mt-1 text-sm font-thin text-gray-500 dark:text-gray-400">chikangatakudzwa@outlook.com</p>
+      <button id="menu-toggle" class="inline-flex items-center px-3 py-2 text-white hover:text-gray-200 hover:border-gray-200 focus:outline-none focus:ring focus:ring-gray-300" aria-label="Toggle navigation">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+  </div>
+  
+  <div class="flex flex-col items-center justify-between flex-1 mt-2">
+      <nav id="main-nav" class="">
+          <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-800 dark:text-gray-200" href="#">
+              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+              <span class="mx-4 font-medium">Home</span>
+          </a>
+          <a class="flex items-center px-4 py-2 mt-2 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+              </svg>
+              <span class="mx-4 font-medium">Projects</span>
+          </a>
+          <a class="flex items-center px-4 py-2 mt-2 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
+              </svg>
+              <span class="mx-4 font-medium">Services</span>
+          </a>
+          <a class="flex items-center px-4 py-2 mt-2 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+              </svg>
+              <span class="mx-4 font-medium">Skills</span>
+          </a>
+          <a class="flex items-center px-4 py-2 mt-2 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+              </svg>
+              <span class="mx-4 font-medium">Contact</span>
+          </a>
+      </nav>
+  </div>
+</section>
+`;
+
+// Add html to sidebarr - so that i can reuse it in other pages
+document.addEventListener("DOMContentLoaded", function() {
+  const navigationContainer = document.querySelector("#sidebarr");
+  navigationContainer.innerHTML = navigation;
+});
+
+// Runs after document has loaded
+window.addEventListener('DOMContentLoaded', () => {
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    menuToggle.addEventListener('click', function() {
+      mainNav.classList.toggle('hidden');
+    });
+
+    // Method to hide nav on small screens
+    function toggleElement() {
+        if (window.innerWidth < 640) { // hide on screen size sm
+            mainNav.classList.add('hidden');
+        } else {
+            menuToggle.classList.add('hidden');
+        }
+    }
+    window.addEventListener('resize', toggleElement);
+    toggleElement(); // call initially to set the visibility based on screen size
+});

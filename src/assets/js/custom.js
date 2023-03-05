55b1692abe35c56f15f692dcf5d652ccd1d76a35
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-03-05 14:46:58 +0000

    Added page compinents, working on mobile toggle

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,3 +1,5 @@
+// Job Role 
+
 const words = ["Full Stack Web Developer", "UI/UX Designer", "DevOps and Cloud Native"]; // add as many words as you want
   let i = 0;
   let j = 0;
@@ -22,3 +24,40 @@ const words = ["Full Stack Web Developer", "UI/UX Designer", "DevOps and Cloud N
     }
   };
   typewriter();
+
+// My Projects
+
+// const nextButton = document.querySelector('#next-button');
+// const grid = document.getElementById('#grid');
+// // const gridItems = document.querySelectorAll('.grid-item');
+
+// function toggleNextButtonVisibility() {
+//   if (grid.children.length > 6) {
+//     nextButton.classList.remove('hidden');
+//   } else {
+//     nextButton.classList.add('hidden');
+//   }
+// }
+
+// toggleNextButtonVisibility();
+
+// // listen for changes to the grid items and toggle the visibility of the next button accordingly
+// const gridObserver = new MutationObserver(toggleNextButtonVisibility);
+// gridObserver.observe(grid, { childList: true });
+
+// Mobile Menu
+
+const menuButton = document.getElementById('menu-button');
+
+menuButton.addEventListener('click', () => {
+  const firstIcon = menuButton.querySelector('svg:first-child');
+  console.log('Runnin');
+  const secondIcon = menuButton.querySelector('svg:last-child');
+  if (firstIcon.style.display === 'block') {
+    firstIcon.style.display = 'none';
+    secondIcon.style.display = 'block';
+  } else {
+    firstIcon.style.display = 'block';
+    secondIcon.style.display = 'none';
+  }
+});

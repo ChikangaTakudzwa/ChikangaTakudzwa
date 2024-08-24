31846dd761ae24ac3cf3909744747394d31bf809
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2024-08-25 00:27:41 +0200

    updated image path with urls

--- app/components/index/heroSection.tsx
+++ app/components/index/heroSection.tsx
@@ -1,29 +1,42 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function HeroSection() {
-    return (
-        <section className="text-gray-600 body-font page-padding section-width">
-            <div className="container bg-gray-100 mx-auto flex py-16 md:flex-row mt-10 rounded-lg flex-col items-center">
-                <div className="lg:max-w-lg flex flex-row justify-center lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                    <Image
-                        src="/avi.png"
-                        alt="Takudzwa Chikanga Profile Photo"
-                        width={400}
-                        height={80}
-                        className="circle-img"
-                        blurDataURL="data:..." 
-                        placeholder="blur" // Optional blur-up while loading
-                    />
-                </div>
-                <div className="lg:flex-grow md:w-1/2 md:px-8  flex flex-col md:items-start md:text-left items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Hi, Call Me TK</h1>
-                    <p className="mb-8 leading-relaxed">Takudzwa Chikanga is a professional software developer who has been in the IT software development field for more than four years, He has had the chance to work with both international businesses and IT teams from different domains. TK is a seasoned developer in many different technologies and fields, but AI and cybersecurity are two areas that he is most interested in.</p>
-                    <div className="flex justify-center">
-                        <button className="inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">Hire Me</button>
-                        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">My Projects</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="text-gray-600 body-font page-padding section-width">
+      <div className="container bg-gray-100 mx-auto flex py-16 md:flex-row mt-10 rounded-lg flex-col items-center">
+        <div className="lg:max-w-lg flex flex-row justify-center lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <Image
+            src="https://ivesexid.sirv.com/Portfolio/2024-Portfolio/avi.png"
+            alt="Takudzwa Chikanga Profile Photo"
+            width={400}
+            height={80}
+            className="circle-img"
+            blurDataURL="data:..."
+            placeholder="blur" // Optional blur-up while loading
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 md:px-8  flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            Hi, Call Me TK
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Takudzwa Chikanga is a professional software developer who has been
+            in the IT software development field for more than four years, He
+            has had the chance to work with both international businesses and IT
+            teams from different domains. TK is a seasoned developer in many
+            different technologies and fields, but AI and cybersecurity are two
+            areas that he is most interested in.
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">
+              Hire Me
+            </button>
+            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              My Projects
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/components/index/myCertsSection.tsx
+++ app/components/index/myCertsSection.tsx
@@ -1,83 +1,103 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const certs = [
-    {
-        image: "/certs/azure_cert.png",
-        alt: "Microsoft Azure Fundamentals Certification",
-        title: "Microsoft Azure",
-        category: "Cloud Computing",
-        verify: "",
-    },
-    {
-        image: "/certs/py4ds_cert.png",
-        alt: "Python For Data Science",
-        title: "Python For Data Science",
-        category: "Data Science",
-        verify: "",
-    },
-    {
-        image: "/certs/containers_k8s_essen_cert.png",
-        alt: "Containers & K8S Essentials",
-        title: "Containers & K8S Essentials",
-        category: "DevOps",
-        verify: "",
-    },
-    {
-        image: "/certs/uplimit_cert.jpeg",
-        alt: "Building AI Products w/ OpenAI Certification",
-        title: "Building AI Products w/ OpenAI",
-        category: "AI/ML",
-        verify: "",
-    },
-    {
-        image: "/certs/ibm_devops_cert.jpeg",
-        alt: "IBM DevOps and SE Professional Certificate",
-        title: "IBM DevOps Professional Certificate",
-        category: "DevOps",
-        verify: "",
-    },
+  {
+    image:
+      "https://ivesexid.sirv.com/Portfolio/2024-Portfolio/certs/azure_cert.png",
+    alt: "Microsoft Azure Fundamentals Certification",
+    title: "Microsoft Azure",
+    category: "Cloud Computing",
+    verify: "",
+  },
+  {
+    image:
+      "https://ivesexid.sirv.com/Portfolio/2024-Portfolio/certs/py4ds_cert.png",
+    alt: "Python For Data Science",
+    title: "Python For Data Science",
+    category: "Data Science",
+    verify: "",
+  },
+  {
+    image:
+      "https://ivesexid.sirv.com/Portfolio/2024-Portfolio/certs/containers_k8s_essen_cert.png",
+    alt: "Containers & K8S Essentials",
+    title: "Containers & K8S Essentials",
+    category: "DevOps",
+    verify: "",
+  },
+  {
+    image:
+      "https://ivesexid.sirv.com/Portfolio/2024-Portfolio/certs/uplimit_cert.jpeg",
+    alt: "Building AI Products w/ OpenAI Certification",
+    title: "Building AI Products w/ OpenAI",
+    category: "AI/ML",
+    verify: "",
+  },
+  {
+    image:
+      "https://ivesexid.sirv.com/Portfolio/2024-Portfolio/certs/ibm_devops_cert.jpeg",
+    alt: "IBM DevOps and SE Professional Certificate",
+    title: "IBM DevOps Professional Certificate",
+    category: "DevOps",
+    verify: "",
+  },
 ];
 
 export default function Certs() {
-    return (
-        <section className="bg-gray-900 text-gray-50 mt-20 body-font section-width">
-            <div className="flex flex-col page-padding">
-                <div className="flex flex-col text-center w-full mt-24">
-                    <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Explore</h2>
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-50">My Certifications</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't</p>
-                </div>
-                <div className="flex flex-wrap">
-                    <Certifications />
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-gray-900 text-gray-50 mt-20 body-font section-width">
+      <div className="flex flex-col page-padding">
+        <div className="flex flex-col text-center w-full mt-24">
+          <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+            Explore
+          </h2>
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-50">
+            My Certifications
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            haven't
+          </p>
+        </div>
+        <div className="flex flex-wrap">
+          <Certifications />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export function Certifications() {
-    return (
-        <section className="text-gray-600 body-font">
-            <div className="container py-24">
-                <div className="flex flex-wrap">
-                    {certs.map((cert, index) => (
-                    <div key={index} className="md:w-1/3 p-2">
-                        <div className="flex flex-col p-1 mb-8 bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-                            <img className="w-full h-auto rounded-t-xl" src={cert.image} alt={cert.alt} />
-                            <div className="p-1">
-                                <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                                <span className="text text-xs bg-gray-900 text-white py-1 px-2 rounded-lg">{cert.category}</span><br />
-                                {cert.title}
-                                </h3>
-                                <p className="mt-5 text-xs text-gray-500 dark:text-neutral-500">
-                                Verify
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    ))}
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container py-24">
+        <div className="flex flex-wrap">
+          {certs.map((cert, index) => (
+            <div key={index} className="md:w-1/3 p-2">
+              <div className="flex flex-col p-1 mb-8 bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                <img
+                  className="w-full h-auto rounded-t-xl"
+                  src={cert.image}
+                  alt={cert.alt}
+                />
+                <div className="p-1">
+                  <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                    <span className="text text-xs bg-gray-900 text-white py-1 px-2 rounded-lg">
+                      {cert.category}
+                    </span>
+                    <br />
+                    {cert.title}
+                  </h3>
+                  <p className="mt-5 text-xs text-gray-500 dark:text-neutral-500">
+                    Verify
+                  </p>
                 </div>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }

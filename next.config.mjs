b3d6cb69729f0c2ff28a0ea0bfebbbb35e7c1192
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2024-08-24 23:42:20 +0200

    added config in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enable static export mode
+  basePath: "/ChikangaTakudzwa", // Set this to your GitHub repository name if not in the root
+  images: {
+    unoptimized: true, // Disables server-side image optimization for static export
+  },
+};
 
 export default nextConfig;
